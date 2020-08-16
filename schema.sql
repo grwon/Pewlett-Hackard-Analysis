a4e4ceded504e75eff8ca5da1a0adde1e3ae7dc1
Grace Wong <grwong@omers.com>
2020-08-16 19:50:44 -0400

    Adding retirement_info.csv

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
 	dept_no VARCHAR(4) NOT NULL,
-	dept_name VARCHAR(40) NOT NULL,
+	dept_name VARCHAR(60) NOT NULL,
 	PRIMARY KEY (dept_no),
 	UNIQUE (dept_name)
 );
@@ -17,7 +17,7 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -36,8 +36,8 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
 	dept_no VARCHAR NOT NULL,
-    emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -54,4 +54,28 @@ CREATE TABLE titles (
   PRIMARY KEY (emp_no, title, from_date)
 );
 
-SELECT * FROM departments;
+SELECT * FROM employees;
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1953-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31'
+
+-- Retirement eligibility
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
